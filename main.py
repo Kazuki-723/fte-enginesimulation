f264cb864b150d7507d553ee0722f5ae426d13d1
Kazuki.T <tomita.kazuki.r3@dc.tohoku.ac.jp>
2026-01-24 22:42:35 +0900

    add json file loader

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from pythonfiles.rocket_simulation import RocketSimulation
+from inputprograms.rocket_simulation import RocketSimulation
+from inputprograms.interp_density import OxidizerDatabase
+from inputprograms.importjson import JsoncLoader
 
-if __name__ == '__main__':
-    sim = RocketSimulation()
 
-    # 初期状態のみ検証
-    #sim.run_initialstate()
+if __name__ == "__main__":
+    print("input jsonc filename(examplr.jsonc):")
+    filename = input("> ").strip()
 
-    # 全部回す
-    sim.run_simulation()
+    try:
+        loader = JsoncLoader(filename)
+        values = loader.load()
+    except Exception as e:
+        print(f"loading error: {e}")
+        exit(1)
+
+    # values に JSONC の内容が dict として入っている
+    print("loading values:")
+    for key, val in values.items():
+        print(f"{key} = {val}")
+
+    # Ptからrho_oxを計算
+    ox_db = OxidizerDatabase()
+    ox_calc_result = ox_db.get_density(values["Pt_init"])
+    rho_ox = float(ox_calc_result.split(":")[-1].replace("kg/m³", "").strip())
+    print(rho_ox)
